6a1642feedfb8019f13693bbeeed39af66c433f3
Zach Preston <zachattack6089@gmail.com>
2025-10-12 18:02:52 -0700

    Copied better fetch code.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const status = ref('N/A')
+const server_status = ref('N/A')
 
-async function getStatus() {
-    status.value = 'Loading...'
+async function get_status() {
+    server_status.value = 'Loading...'
 
-    const url = 'https://website-backend-eight-tau.vercel.app/status'
-    const response = await fetch(url)
-    const result = await response.json()
-
-    status.value = result['status']
+    const api_url = import.meta.env['VITE_BACKEND_URL']
+    const status_url = `${api_url}/status`
+    try {
+        const response = await fetch(status_url)
+        const result = await response.json()
+        server_status.value = result['status']
+    } catch (e: any) {
+        console.error(e.message)
+        server_status.value = 'Fetch error :('
+    }
 }
 </script>
 
 <template>
     <h2>Hello world! :3</h2>
-    <p>Status: {{ status }}</p>
-    <button @click="getStatus">Get Status</button>
+    <p>Status: {{ server_status }}</p>
+    <button @click="get_status">Get Status</button>
 </template>
 
 <style scoped></style>
