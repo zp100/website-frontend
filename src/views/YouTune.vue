74a45a7397b557fb9656da6edcfb718db22ef509
Zach Preston <zachattack6089@gmail.com>
2025-10-19 16:36:27 -0700

    Added another example page.

--- src/views/YouTune.vue
+++ src/views/YouTune.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const track_list = ref([])
+
+onMounted(async () => {
+    const api_url = import.meta.env.VITE_BACKEND_URL
+    const tracks_url = `${api_url}/youtune/tracks`
+    const response = await fetch(tracks_url)
+    const result = await response.json()
+    
+    track_list.value = result['tracks']
+})
+</script>
+
 <template>
     Hello from YouTune page!
+
+    <h4>Tracks</h4>
+    <ul>
+        <template v-for="track in track_list">
+            <li>{{ track }}</li>
+        </template>
+    </ul>
 </template>
