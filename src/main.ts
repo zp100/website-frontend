06d0aff9f22c4886e36196635467026389ce39c4
Zach Preston <zachattack6089@gmail.com>
2026-03-16 01:11:43 -0700

    Added controls.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCircleRight, faSquareCaretLeft, faTrashCan } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 
+library.add(faCircleRight, faSquareCaretLeft, faTrashCan)
+
 const app = createApp(App)
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
--- src/pangram/Pangram.vue
+++ src/pangram/Pangram.vue
@@ -2,6 +2,31 @@
 import { onMounted, ref } from 'vue'
 
 
+// Update guess when keys are pressed.
+const guess = ref<string[]>([])
+window.addEventListener('keydown', (ev: KeyboardEvent) => {
+    if (ev.key === 'Backspace') {
+        guess.value.pop()
+        return
+    }
+
+    if (ev.key === 'Enter') {
+        submit_guess()
+        return
+    }
+
+    if (ev.repeat) {
+        return
+    }
+
+    const isLetter = /^[A-Za-z]$/.test(ev.key)
+    if (isLetter) {
+        guess.value.push(ev.key)
+        return
+    }
+})
+
+
 let word_list: string[]
 let puzzle_list: string[]
 onMounted(async () => {
@@ -27,17 +52,19 @@ async function get_word_list(): Promise<WordListResponse> {
 }
 
 
-let puzzle_letters: string[]
-let key_letter: string
-let answer_word_list: string[]
+const puzzle_letters = ref<string[]>([])
+const key_letter = ref('')
+const answer_word_list = ref<string[]>([])
 function set_puzzle(): void {
     while (true) {
-        puzzle_letters = sample(puzzle_list).split('')
-        key_letter = sample(puzzle_letters)
-        answer_word_list = word_list.filter((word) => is_valid(word, puzzle_letters, key_letter))
+        const try_puzzle_letters = sample(puzzle_list).split('')
+        const try_key_letter = sample(try_puzzle_letters)
+        const try_answer_word_list = word_list.filter((word) => is_valid(word, try_puzzle_letters, try_key_letter))
 
-        if (answer_word_list.length >= 10 && answer_word_list.length <= 50) {
-            console.log({ puzzle_letters, key_letter, answer_word_list })
+        if (try_answer_word_list.length >= 10 && try_answer_word_list.length <= 50) {
+            puzzle_letters.value = try_puzzle_letters
+            key_letter.value = try_key_letter
+            answer_word_list.value = try_answer_word_list
             return
         }
     }
@@ -72,32 +99,76 @@ function is_valid(word: string, puzzle_letters: string[], key_letter: string): b
 }
 
 
-// Update guess when keys are pressed.
-const guess = ref<string[]>([])
-window.addEventListener('keydown', (ev: KeyboardEvent) => {
-    if (ev.key === 'Backspace') {
-        guess.value.pop()
+function submit_guess(): void {
+    const guess_letters = guess.value
+    guess.value = []
+
+    if (!guess_letters.includes(key_letter.value)) {
+        window.alert('must contain key letter')
         return
     }
 
-    if (ev.repeat) {
+    if (guess_letters.length < 4) {
+        window.alert('too short lmao')
         return
     }
 
-    const isLetter = /^[A-Za-z]$/.test(ev.key)
-    if (isLetter) {
-        guess.value.push(ev.key)
+    const is_bad_input = guess_letters.some((letter) => !puzzle_letters.value.includes(letter))
+    if (is_bad_input) {
+        window.alert('invalid letters bruh')
         return
     }
-})
+
+    const guess_word = guess_letters.join('')
+    if (word_list.includes(guess_word)) {
+        window.alert('word found!')
+        return
+    } else {
+        window.alert('not in word list')
+        return
+    }
+}
+
+
 </script>
 
 
 
 <template>
     <div id="game">
-        <div v-for="letter in guess" class="guess-letter">
-            {{ letter.toLocaleUpperCase() }}
+        <div id="input">
+            <div
+                v-for="letter in guess"
+                class="guess-letter"
+                :class="{ 'key-letter': letter === key_letter, 'invalid-letter': !puzzle_letters.includes(letter) }"
+            >
+                {{ letter.toLocaleUpperCase() }}
+            </div>
+
+            <div id="cursor" class="guess-letter"></div>
+        </div>
+
+        <div id="options">
+            <button
+                v-for="letter in puzzle_letters"
+                class="option-letter"
+                :class="{ 'key-letter': letter === key_letter }"
+                @click="guess.push(letter)"
+            >
+                {{ letter.toLocaleUpperCase() }}
+            </button>
+        </div>
+
+        <div id="actions">
+            <button class="action-btn" @click="guess.length = 0">
+                <font-awesome-icon icon="fa-regular fa-trash-can" />
+            </button>
+            <button class="action-btn" @click="guess.pop()">
+                <font-awesome-icon icon="fa-regular fa-square-caret-left" />
+            </button>
+            <button class="action-btn" @click="submit_guess()">
+                <font-awesome-icon icon="fa-regular fa-circle-right" />
+            </button>
         </div>
     </div>
 </template>
@@ -110,14 +181,83 @@ window.addEventListener('keydown', (ev: KeyboardEvent) => {
     height: 100vh;
     background-color: #111;
     display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+#input {
+    margin: 10  px;
+    display: flex;
     flex-flow: row nowrap;
     justify-content: center;
     align-items: center;
+    gap: 5px;
 }
 
 .guess-letter {
     color: white;
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: large;
+    font-size: xx-large;
+
+    &.key-letter {
+        color: #ff0;
+    }
+
+    &.invalid-letter {
+        color: #333;
+    }
+}
+
+#cursor {
+    height: 1.2em;
+    width: 4px;
+    border-radius: 2px;
+    background-color: #333;
+}
+
+button {
+    border: none;
+    border-radius: 5px;
+    background-color: #222;
+    color: #ddd;
+    cursor: pointer;
+
+    &:hover {
+        outline: 2px solid #444;
+    }
+}
+
+#options {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+}
+
+.option-letter {
+    width: 50px;
+    height: 50px;
+    font-size: xx-large;
+
+    &.key-letter {
+        color: #ff0;
+    }
+}
+
+#actions {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+}
+
+.action-btn {
+    width: 130px;
+    height: 50px;
+    font-size: x-large;
 }
 </style>
