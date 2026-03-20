2aec1f1d27bcfc7d61a4ff7ebb15c9be2b73725a
Zach Preston <zachattack6089@gmail.com>
2026-03-19 21:21:58 -0700

    Added button components.

---
+++ src/pangrams/components/ActionButton.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+const props = defineProps<{
+    title: string
+    icon: string
+}>()
+const emit = defineEmits<{
+    click: []
+}>()
+</script>
+
+
+
+<template>
+    <button
+        class="action-btn"
+        :title="title"
+        @keydown.prevent=""
+        @click="emit('click')"
+    >
+        <FontAwesomeIcon :icon="`fa-solid fa-${icon}`" />
+    </button>
+</template>
+
+
+
+<style scoped>
+.action-btn {
+    width: var(--wide-button-unit);
+    height: var(--button-unit);
+    font-size: x-large;
+}
+</style>
---
+++ src/pangrams/components/OptionButton.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+const props = defineProps<{
+    letter: string
+    is_key_letter: boolean
+    count: number | undefined
+}>()
+const emit = defineEmits<{
+    click: []
+}>()
+</script>
+
+
+
+<template>
+    <button
+        class="option-btn"
+        :class="{ 'key-letter': is_key_letter }"
+        @keydown.prevent=""
+        @click="emit('click')"
+    >
+        {{ letter.toLocaleUpperCase() }}
+        <div class="letter-counter">
+            <div>
+                {{ count }}
+            </div>
+        </div>
+    </button>
+</template>
+
+
+
+<style scoped>
+.option-btn {
+    position: relative;
+    width: var(--button-unit);
+    height: var(--button-unit);
+    font-size: x-large;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+
+    &.key-letter {
+        color: yellow;
+    }
+}
+
+
+.letter-counter {
+    position: absolute;
+    width: 100%;
+    height: 0;
+    bottom: -4px;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+
+    div {
+        width: max-content;
+        color: var(--off-white);
+        background-color: var(--border-color);
+        padding: 0 3px;
+        border-radius: var(--roundness);
+
+        font-size: small;
+    }
+}
+</style>
