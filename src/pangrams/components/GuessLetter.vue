ba818cf7bdd9402e6e9b9900e18ac274fc5f8b12
Zach Preston <zachattack6089@gmail.com>
2026-03-19 21:39:33 -0700

    Added guess letter component, font size tweaks.

---
+++ src/pangrams/components/GuessLetter.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const props = defineProps<{
+    letter: string
+    is_key: boolean
+    is_invalid: boolean
+}>()
+</script>
+
+
+
+<template>
+    <div
+        class="guess-letter"
+        :class="{ 'key-letter': is_key, 'invalid-letter': is_invalid }"
+    >
+        {{ letter.toLocaleUpperCase() }}
+    </div>
+</template>
+
+
+
+<style scoped>
+.guess-letter {
+    color: white;
+    font-size: var(--guess-font-size);
+
+    &.key-letter {
+        color: yellow;
+    }
+
+    &.invalid-letter {
+        color: var(--border-color);
+    }
+}
+</style>
